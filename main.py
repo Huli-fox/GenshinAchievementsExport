06649a6451947c798621b9954e99b801e983fa42
Huli_fox <74598446+Huli-fox@users.noreply.github.com>
2022-01-18 20:55:12 +0800

    还很混乱，不能用

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-# import time
-# import json
-# from screenshot import jamroll_screenshot
-# with open('config.json','r',encoding='utf-8') as r:
-#     config=json.load(r)
-#     area = config['area']
-# time.sleep(2)
-# jamroll_screenshot.main(area)
+import json
+import json
+from src import roll_screenshot
 
+with open('config.json', 'r', encoding='utf-8') as r:
+    config = json.load(r)
+    area = config['area']
+roll_screenshot.main()
 
-from paddleocr import PaddleOCR, draw_ocr
-
-# Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
-# 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
-ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
-img_path = './screenshots/3.png'
-result = ocr.ocr(img_path, cls=True)
-for line in result:
-    print(line)
-    with open('result.txt', 'a+', encoding='utf-8') as f:
-        f.write(line[1][0] + '\n')
+# from paddleocr import PaddleOCR, draw_ocr
+#
+# # Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
+# # 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
+# ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
+# img_path = './screenshots/.png'
+# result = ocr.ocr(img_path, cls=True)
+# for line in result:
+#     print(line)
+#     with open('result.txt', 'a+', encoding='utf-8') as f:
+#         f.write(line[1][0] + '\n')
 
 # # 显示结果
 # from PIL import Image
