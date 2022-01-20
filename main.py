86bb144f6ef27af21da7e41fb800ba45ee648ca7
Huli_fox <74598446+Huli-fox@users.noreply.github.com>
2022-01-20 14:55:59 +0800

    应该勉强能跑了（吧）

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-import json
-import os
-import sys
-from src import self_check, roll_screenshot
+from src import self_check, roll_screenshot, export
 
-
-# self_check.main()
-# print('[Self Check]初始化完成！')
-# roll_screenshot.main()
-
-from src import export
-
-export.main()
-
-
+self_check.main()
+print('[Self Check]初始化完成！')
+roll_screenshot.main()
+export.write_txt()
+export.write_excel()
--- src/export.py
+++ src/export.py
@@ -1,6 +1,8 @@
 import os
-from PIL import Image
-from paddleocr import PaddleOCR, draw_ocr
+import re
+import xlwt
+import linecache
+from paddleocr import PaddleOCR
 
 
 def getfiles():
@@ -8,7 +10,7 @@ def getfiles():
     return file_list
 
 
-def main():
+def write_txt():
     # Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
     # 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
     ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
@@ -20,11 +22,66 @@ def main():
             print(line)
             with open('result.txt', 'a+', encoding='utf-8') as f:
                 f.write(line[1][0] + '\n')
-        # 显示结果
-        image = Image.open(img_path).convert('RGB')
-        boxes = [line[0] for line in result]
-        txts = [line[1][0] for line in result]
-        scores = [line[1][1] for line in result]
-        im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/simfang.ttf')
-        im_show = Image.fromarray(im_show)
-        im_show.save('results/%s' % n)
+
+
+def write_excel():
+    counter = 1
+    names = []
+    contents = []
+    isCompletes = []
+    progresses = []
+    dates = []
+    findExData = re.compile(r'总计(.*?)\n')
+
+    def check():
+        if counter >= sum(1 for _ in open('result.txt', encoding='utf-8')):
+            return False
+        else:
+            return True
+
+    while True:
+        name = linecache.getline('result.txt', counter).rstrip()
+        names.append(name)
+        is_complete = linecache.getline('result.txt', counter + 1).rstrip()
+        if is_complete == '达成':
+            isCompletes.append(True)
+            progress = re.findall(findExData, linecache.getline('result.txt', counter + 2))
+            if progress:
+                progresses.append('总计%s' % progress[0])
+                contents.append(linecache.getline('result.txt', counter + 3).rstrip())
+                dates.append(linecache.getline('result.txt', counter + 4).rstrip())
+                counter += 5
+                if not check():
+                    break
+            else:
+                progresses.append('null')
+                contents.append(linecache.getline('result.txt', counter + 2).rstrip())
+                dates.append(linecache.getline('result.txt', counter + 3).rstrip())
+                counter += 4
+                if not check():
+                    break
+        else:
+            isCompletes.append(False)
+            progresses.append(linecache.getline('result.txt', counter + 1).rstrip())
+            contents.append(linecache.getline('result.txt', counter + 2).rstrip())
+            dates.append('null')
+            counter += 3
+            if not check():
+                break
+    print(names, contents, isCompletes, progresses, dates)
+    book = xlwt.Workbook(encoding='utf-8', style_compression=0)
+    sheet1 = book.add_sheet('sheet1', cell_overwrite_ok=True)
+    title = ['序号', '名称', '内容', '是否完成', '进度', '日期']
+    for i in range(6):
+        sheet1.write(0, i, title[i])
+    a = 0
+    for name in names:
+        sheet1.write(a + 1, 0, a + 1)
+        sheet1.write(a + 1, 1, name)
+        sheet1.write(a + 1, 2, contents[a])
+        sheet1.write(a + 1, 3, isCompletes[a])
+        sheet1.write(a + 1, 4, progresses[a])
+        sheet1.write(a + 1, 5, dates[a])
+        a += 1
+    book.save('result.xlsx')
+    os.remove('result.txt')
